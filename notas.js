1919106da8e67d912f79bb943669bff5bc403570
NatalyUwU <lauritatelle.03@gmail.com>
2024-10-09 20:59:45 -0500

    Se realizo los archivos CSS modularizados

--- notas.js
+++ notas.js
@@ -278,19 +278,101 @@ overflow: hidden;          ocultar un elemento
 
 
 
-------------------------------------------------------------------------FLEXBOX
+-------------------------------------------------------------------FLEXBOX
 -------DISPLAY
 display: inline-flex;      ------se alinean en horizontalmente
 display: flex           ----------se alinean
 
-------DIRECCION
-flex-direction: row;       ---horizontalmente
-flex-direction: row-reverse;          ---horizontal se invierte el orden
+----------------------------------------------------------------------justify-content        cuando es una columna, justify-content cambia a vertical y align-items a horizontal.
+la cual alinea elementos horizontalmente y acepta los siguientes valores:
 
-flex-direction: column;          ---------como columna
- flex-direction: column-reverse;        ---------como columna se invierte el orden
+flex-start: Alinea elementos al lado izquierdo del contenedor.
+flex-end: Alinea elementos al lado derecho del contenedor.
+center: Alinea elementos en el centro del contenedor.
+space-between: Muestra elementos con la misma distancia entre ellos.
+space-around: Muestra elementos con la misma separación alrededor de ellos.
 
- flex-wrap: nowwrap;         -----muestra todos los elementos en una misma linea
+ ej: justify-content: flex-end;
+
+----------------------------------------------------------------------align-items              cuando es una columna, justify-content cambia a vertical y align-items a horizontal.
+Esta propiedad CSS alinea elementos verticalmente y acepta los siguientes valores:
+
+flex-start: Alinea elementos a la parte superior del contenedor.
+flex-end: Alinea elementos a la parte inferior del contenedor.
+center: Alinea elementos en el centro (verticalmente hablando) del contenedor.
+baseline: Muestra elementos en la línea base del contenedor
+stretch: Elementos se estiran para ajustarse al contenedor.
+
+ej:align-items: flex-end;
+
+----------------------------------------------------------------------flex-direction
+Esta propiedad CSS define la dirección de los elementos en el contenedor, y acepta los siguientes valores:
+
+row: Elementos son colocados en la misma dirección del texto.
+row-reverse: Elementos son colocados en la dirección opuesta al texto.
+column: Elementos se colocan de arriba hacia abajo.
+column-reverse: Elementos se colocan de abajo hacia arriba.
+
+
+----------------------------------------------------------------------order
+En esos casos, nosotros podemos aplicar la propiedad order a elementos individuales. 
+Por defecto, los elementos tienen un valor 0, pero nosotros podemos usar esta propiedad para establecerlo a un número entero positivo o negativo.
+
+ej:
+#pond {
+  display: flex;
+}
+
+.yellow {
+order: 2;
+}
+
+----------------------------------------------------------------------align-self
+Esta propiedad acepta los mismos valores de align-items y sus valores son usados para un elemento específico
+
+ej:
+#pond {
+  display: flex;
+  align-items: flex-start;
+}
+
+.yellow {
+align-self: flex-end;
+}
+
+----------------------------------------------------------------------flex-wrap
+ la cual acepta los siguientes valores:
+
+ nowrap: Cada elemento se ajusta en una sola línea.
+wrap: los elementos se envuelven alrededor de líneas adicionales.
+wrap-reverse: Los elementos se envuelven alrededor de líneas adicionales en reversa.
+
+----------------------------------------------------------------------flex-flow
+a cual fue creada para combinar flex-direction y flex-wrap 
+Por ejemplo, puedes usar flex-flow para establecer filas y envolverlas.
+
+Trata de usar flex-flow para volver a realizar el nivel anterior.
+
+ej:
+#pond {
+  display: flex;
+flex-flow:column wrap;
+}
+
+----------------------------------------------------------------------align-content
+ara establecer como múltiples líneas están separadas una de otra. Esta propiedad acepta los siguientes valores:
+
+flex-start: Las líneas se posicionan en la parte superior del contenedor.
+flex-end: Las líneas se posicionan en la parte inferior del contenedor.
+center: Las líneas se posicionan en el centro (verticalmente hablando) del contenedor.
+space-between: Las líneas se muestran con la misma distancia entre ellas.
+space-around: Las líneas se muestran con la misma separación alrededor de ellas.
+stretch: Las líneas se estiran para ajustarse al contenedor.
+
+align-content determina el espacio entre las líneas,
+mientras que align-items determina como los elementos en su conjunto están alineados dentro del contenedor. Cuando hay solo una línea, align-content no tiene efecto.
+
+*/
 
 
 
