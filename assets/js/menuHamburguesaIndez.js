7e306931651c07aeb3523554566e62e12cdba7c5
NatalyUwU <lauritatelle.03@gmail.com>
2024-10-26 13:15:14 -0500

    se mejoro el css y js de index

--- assets/js/menuHamburguesaIndez.js
+++ assets/js/menuHamburguesaIndez.js
@@ -1,76 +1,98 @@
 //abrir y cerrar menu hamburguesa
-const nav = document.querySelector ("#nav");
-const abrir = document.querySelector ("#abrir");
-const cerrar = document.querySelector ("#cerrar");
+const nav = document.getElementById("nav");
+const abrir = document.getElementById("abrir");
+const cerrar = document.getElementById("cerrar");
+
 
 //menu servicios
-const despliegueServicios = document.querySelector(".despliegueServicios");
+const despliegueServicios = document.getElementById("despliegueServicios");
 const elementosServicios = document.querySelectorAll('#listaServicios li');
 
 //menu productos
-const despliegueProductos = document.querySelector(".despliegueProductos");
+const despliegueProductos = document.getElementById("despliegueProductos");
 const elementosProductos = document.querySelectorAll('#listaProductos li');
 
 //menu Autocuidado
-const despliegueAutocuidado = document.querySelector(".despliegueAutocuidado");
+const despliegueAutocuidado = document.getElementById("despliegueAutocuidado");
 const elementosAutocuidado = document.querySelectorAll('#listaAutocuidado li');
 
 //menu Reseñas
-const despliegueReseñas = document.querySelector(".despliegueReseñas");
+const despliegueReseñas = document.getElementById("despliegueReseñas");
 const elementosReseñas = document.querySelectorAll('#listaReseñas li');
 
 
 
 
 abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
+  nav.classList.add("visible");
 });
 
 cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
+  nav.classList.remove("visible");
 });
 
 //despliegue menu servicios
-despliegueServicios.addEventListener('click', function() {
-    elementosServicios.forEach(function(li) {
-      if (li.style.display === 'none') {
-        li.style.display = 'block'; 
-      } else {
-        li.style.display = 'none'; 
-      }
-    });
+
+elementosServicios.forEach(function (li) {
+  li.style.display = 'none';
+});
+
+despliegueServicios.addEventListener('click', function () {
+  elementosServicios.forEach(function (li) {
+    if (li.style.display === 'none') {
+      li.style.display = 'block';
+    } else {
+      li.style.display = 'none';
+    }
   });
+});
 
 //despliegue menu productos
-  despliegueProductos.addEventListener('click', function() {
-    elementosProductos.forEach(function(li) {
-      if (li.style.display === 'none') {
-        li.style.display = 'block'; 
-      } else {
-        li.style.display = 'none'; 
-      }
-    });
-  });
+
+elementosProductos.forEach(function (li) {
+  li.style.display = 'none';
+});
 
 
-  //despliegue menu Autocuidado
-  despliegueAutocuidado.addEventListener('click', function() {
-    elementosAutocuidado.forEach(function(li) {
-      if (li.style.display === 'none') {
-        li.style.display = 'block'; 
-      } else {
-        li.style.display = 'none'; 
-      }
-    });
-  });
+despliegueProductos.addEventListener('click', function () {
+  elementosProductos.forEach(function (li) {
 
-   //despliegue menu Reseñas
-   despliegueReseñas.addEventListener('click', function() {
-    elementosReseñas.forEach(function(li) {
-      if (li.style.display === 'none') {
-        li.style.display = 'block'; 
-      } else {
-        li.style.display = 'none'; 
-      }
-    });
+    if (li.style.display === 'none') {
+      li.style.display = 'block';
+    } else {
+      li.style.display = 'none';
+    }
   });
+});
+
+
+//despliegue menu Autocuidado
+elementosAutocuidado.forEach(function (li) {
+  li.style.display = 'none';
+});
+
+despliegueAutocuidado.addEventListener('click', function () {
+  elementosAutocuidado.forEach(function (li) {
+
+    if (li.style.display === 'none') {
+      li.style.display = 'block';
+    } else {
+      li.style.display = 'none';
+    }
+  });
+});
+
+//despliegue menu Reseñas
+elementosReseñas.forEach(function (li) {
+  li.style.display = 'none';
+});
+
+despliegueReseñas.addEventListener('click', function () {
+  elementosReseñas.forEach(function (li) {
+    if (li.style.display === 'none') {
+      li.style.display = 'block';
+    } else {
+      li.style.display = 'none';
+    }
+  });
+});
